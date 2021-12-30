7bbced619fe6abf8030e1c831d482307d97a2305
Adam Oh Zhi Hong <adamohzhihong@gmail.com>
2021-12-30 16:57:41 +0800

    Remove tailwind css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
 import './App.scss';
-import { Body } from './components/Body';
 import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Body />
     </div>
   );
 }
