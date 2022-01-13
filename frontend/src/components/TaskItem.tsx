a0ec5d62f90d2019b1538d2ce5438bc5c115bbef
Adam Oh Zhi Hong <adamohzhihong@gmail.com>
2022-01-13 23:11:34 +0800

    Design individual task item

--- frontend/src/components/TaskItem.tsx
+++ frontend/src/components/TaskItem.tsx
@@ -1,27 +1,43 @@
-import { Box, Typography } from "@mui/material"
-import { useState } from "react"
+import { Box, Checkbox, Chip, ClickAwayListener, Stack, TextField, Typography } from "@mui/material"
+import { useState, useEffect } from "react"
 import './TaskItem.scss'
 
 export const TaskItem = () => {
 
     const [boxShadowValue, setBoxShadowValue] = useState(0)
+    const [isEditing, setIsEditing] = useState(false)
 
-    const handleMouseEnter = () => {
-        setBoxShadowValue(3)
+    const NameEditField = () => {
+        return (
+            <ClickAwayListener onClickAway={() => setIsEditing(false)}>
+                <TextField autoFocus sx={{ width: '100%', paddingRight: 3 }} id="standard-basic" label="" variant="standard" />
+            </ClickAwayListener>
+        )
     }
 
-    const handleMouseLeave = () => {
-        setBoxShadowValue(0)
+    const Name = () => {
+        return (
+            <Typography onClick={() => setIsEditing(true)} variant="body1">Walk the dog!</Typography>
+        )
     }
 
     return (
         <Box 
         sx={{ borderRadius: 1, boxShadow: boxShadowValue }}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        className="task-item"
-        >
-            <Typography variant="body1">Walk the dog!</Typography> 
+        onMouseEnter={() => setBoxShadowValue(3)}
+        onMouseLeave={() => setBoxShadowValue(0)}
+        className="task-item">
+            <Stack direction='row' alignItems='center'>
+                <Checkbox />
+                {isEditing ? <NameEditField /> : <Name />}
+            </Stack>
+            <Stack direction='row' spacing={0.5} >
+                <Chip variant='outlined' size='small' label="Hello"/>
+                <Chip variant='outlined' size='small' label="Hello"/>
+                <Chip variant='outlined' size='small' label="Hello"/>
+                <Chip variant='outlined' size='small' label="Hello"/>
+                <Chip variant='outlined' size='small' label="Hello"/>
+            </Stack>
         </Box>
     )
 }
